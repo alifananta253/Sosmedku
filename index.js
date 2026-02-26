4126f681bcde5c58293b42b2fc0ac329cb5da93a
alifananta253 <131012016+alifananta253@users.noreply.github.com>
2026-02-27 05:16:39 +0700

    Refactor index.js to remove unused code and comments

--- index.js
+++ index.js
@@ -1,92 +1,33 @@
-// index.js
-'use strict'
-
-const express = require('express')
-const httpErrors = require('http-errors')
-const pino = require('pino')
-const pinoHttp = require('pino-http')
+const express = require("express")
+const httpErrors = require("http-errors")
+const pino = require("pino")
+const pinoHttp = require("pino-http")
 
 const app = express()
 const logger = pino()
 
-// Middleware
+// middleware
 app.use(pinoHttp({ logger }))
 app.use(express.json()) // penting untuk JSON body
 
-// Routes
-require('./routes')(app)
+// routes
+require("./routes")(app) 
 
 // 404 handler
 app.use((req, res, next) => {
   next(httpErrors(404, `Route not found: ${req.url}`))
 })
 
-// Error handler
+// error handler
 app.use((err, req, res, next) => {
-  if (err.status >= 500) {
-    logger.error(err)
-  }
+  if (err.status >= 500) logger.error(err)
   res.status(err.status || 500).json({
     messages: [{
-      code: err.code || 'InternalServerError',
-      message: err.message
+      code: err.code || "InternalServerError",
+      message: err.message,
     }]
   })
 })
 
-// Export Express app sebagai handler serverless
-module.exports = app    }
-  }
-  process.on('uncaughtException', unhandledError)
-  process.on('unhandledRejection', unhandledError)
-
-  // Create the express app
-  const app = express()
-
-
-  // Common middleware
-  // app.use(/* ... */)
-  app.use(pinoHttp({ logger }))
-      
-  // Register routes
-  // @NOTE: require here because this ensures that even syntax errors
-  // or other startup related errors are caught logged and debuggable.
-  // Alternativly, you could setup external log handling for startup
-  // errors and handle them outside the node process.  I find this is
-  // better because it works out of the box even in local development.
-  require('./routes')(app, opts)
-
-  // Common error handlers
-  app.use(function fourOhFourHandler (req, res, next) {
-    next(httpErrors(404, `Route not found: ${req.url}`))
-  })
-  app.use(function fiveHundredHandler (err, req, res, next) {
-    if (err.status >= 500) {
-      logger.error(err)
-    }
-    res.status(err.status || 500).json({
-      messages: [{
-        code: err.code || 'InternalServerError',
-        message: err.message
-      }]
-    })
-  })
-
-  // Start server
-  server = app.listen(opts.port, opts.host, function (err) {
-    if (err) {
-      return ready(err, app, server)
-    }
-
-    // If some other error means we should close
-    if (serverClosing) {
-      return ready(new Error('Server was closed before it could start'))
-    }
-
-    serverStarted = true
-    const addr = server.address()
-    logger.info(`Started at ${opts.host || addr.host || 'localhost'}:${addr.port}`)
-    ready(err, app, server)
-  })
-}
-
+// **tidak ada app.listen / ready callback**
+module.exports = app
